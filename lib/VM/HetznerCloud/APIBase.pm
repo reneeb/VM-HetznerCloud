82f99fa391cf194206e82e35f2853c308407bfc7
reneeb <info@perl-services.de>
2023-06-20 16:43:05 +0200

    validate parameters and fix parameter handling

--- lib/VM/HetznerCloud/APIBase.pm
+++ lib/VM/HetznerCloud/APIBase.pm
@@ -31,8 +31,16 @@ has client   => (
 );
 
 sub _request ( $self, $partial_uri, $params = {}, $param_def = {}, $opts = {} ) {
-    if ( $param_def->%* ) {
-        $self->_validate( $params, $param_def );
+
+    my ($validated_params, @errors) = VM::HetznerCloud::Schema->validate(
+        $opts->{oid},
+        $params,
+    );
+
+    $validated_params ||= {};
+
+    if ( @errors ) {
+        croak 'Invalid parameters';
     }
 
     my $method = delete $opts->{type} // 'get';
@@ -42,30 +50,28 @@ sub _request ( $self, $partial_uri, $params = {}, $param_def = {}, $opts = {} ) 
         croak sprintf 'Invalid request method %s', $method;
     }
 
-    $partial_uri =~ s{:(?<mandatory>\w+)\b}{ delete $params->{$+{mandatory}} }xmsge;
+    $partial_uri ||= '';
+    $partial_uri =~ s{:(?<mandatory>\w+)\b}{ ( delete $validated_params->{path}->{$+{mandatory}} ) // '' }xmsge;
+    $partial_uri =~ s{\A/}{};
+
+    my $query = join '&', map{
+        $_ . '=' . url_escape(  delete $validated_params->{query}->{$_} )
+    } grep {
+        $param_def->{$_}->{in} eq 'query' && $validated_params->{query}->{$_}
+    }sort keys $param_def->%*;
 
     my %request_opts;
     if ( $params->%* ) {
-        %request_opts = ( json => $params );
+        %request_opts = ( json => $validated_params->{body} );
     }
-
-    $opts->{query} //= {};
-    my $query = '';
-    if ( $opts->{query}->%* ) {
-        my $query_params = delete $opts->{query};
-
-        $query = join '&', map{ 
-            $_ . '=' . uri_escape($query_params->{$_}) 
-        }sort keys $query_params->%*;
-    }
-
-    $partial_uri .= '?' . $query if $query;
 
     my $uri = join '/', 
         $self->host, 
         $self->base_uri,
         $self->endpoint,
-        $partial_uri;
+        ( $partial_uri ? $partial_uri : () );
+
+    $uri .= '?' . $query if $query;
 
     my $tx = $self->client->$method(
         $uri,
@@ -82,26 +88,6 @@ sub _request ( $self, $partial_uri, $params = {}, $param_def = {}, $opts = {} ) 
 
     return if $response->is_error;
     return $response->json;
-}
-
-sub _validate ( $self, $params, $def ) {
-    for my $param ( sort keys $def->%* ) {
-        if ( $def->{$param}->{required} && !length $params->{$param} ) {
-            die "Mandatory parameter $param not set!";
-        }
-
-        if ( $def->{$param}->{validation} ) {
-            my $error = $self->_validate_param( $params->{$param}, $def->{$param}->{validation} );
-        }
-    }
-
-    return 1;
-}
-
-sub _validate_param ( $self, $value, $format ) {
-    return if !$value;
-
-    return;
 }
 
 1;
